999c10fce885d54c38b1766452d2b068fa353aec
kira s <kira.totti97@gmail.com>
2019-04-25 14:29:17 +0300

    Updates

---
+++ precache-manifest.8cc598bf3c8156f381e835c5cbe33da7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992482ebd145b31b4df3393388a56cf0",
+    "url": "/traffic-code/index.html"
+  },
+  {
+    "revision": "41635bb84f74205690b0",
+    "url": "/traffic-code/static/css/main.551b8521.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/traffic-code/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "41635bb84f74205690b0",
+    "url": "/traffic-code/static/js/main.bd017752.chunk.js"
+  },
+  {
+    "revision": "f235edd5cc6e76edbe6c",
+    "url": "/traffic-code/static/js/runtime~main.ff943b63.js"
+  },
+  {
+    "revision": "eac97d2633551d866e39fa83ff4ec20d",
+    "url": "/traffic-code/static/media/finland.eac97d26.jpg"
+  },
+  {
+    "revision": "97129de002707f8317afa5cb43aa2cf1",
+    "url": "/traffic-code/static/media/france.97129de0.jpg"
+  },
+  {
+    "revision": "83b1b67dd601b7181a36f456ed22233d",
+    "url": "/traffic-code/static/media/germany.83b1b67d.jpg"
+  },
+  {
+    "revision": "9ff77305fc0ced3b2c6e9a103ea62244",
+    "url": "/traffic-code/static/media/italy.9ff77305.jpg"
+  },
+  {
+    "revision": "4696a1858830b029e19994539f330bc0",
+    "url": "/traffic-code/static/media/japan.4696a185.jpg"
+  },
+  {
+    "revision": "5e2e18ca1b033d5123cf7fcb7cfad451",
+    "url": "/traffic-code/static/media/mexico.5e2e18ca.jpg"
+  },
+  {
+    "revision": "f0ef328121ac7dca84de740bd14988ae",
+    "url": "/traffic-code/static/media/thailand.f0ef3281.jpg"
+  },
+  {
+    "revision": "26510eb14ea81beeb15752557ce21203",
+    "url": "/traffic-code/static/media/title.26510eb1.jpg"
+  },
+  {
+    "revision": "f709e17aad200252d761cbe09ef4740c",
+    "url": "/traffic-code/static/media/uae.f709e17a.jpg"
+  },
+  {
+    "revision": "cfeb1ff9a38befd008150c95b4972e3c",
+    "url": "/traffic-code/static/media/usa.cfeb1ff9.jpg"
+  }
+]);
